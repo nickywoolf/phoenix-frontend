f5ddfaebc098ff95c578cea65aecc625e8014091
Nicky Woolf <woolfnicky@gmail.com>
2019-08-17 06:38:17 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,9 +5,16 @@ defmodule PhoenixFrontend.MixProject do
     [
       app: :phoenix_frontend,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "",
+      source_url: "https://github.com/nickywoolf/phoenix-frontend",
+      package: package(),
+      description: """
+      Replaces Phoenix framework assets directory with
+      preconfigured frontend frameworks.
+      """
     ]
   end
 
@@ -23,6 +30,14 @@ defmodule PhoenixFrontend.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nickywoolf/phoenix-frontend"},
+      files: ~w(assets lib test .formatter.exs .gitignore mix.exs README.md)
     ]
   end
 end
